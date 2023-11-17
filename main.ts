c97e71c45b6c478239d09b9ceedb60d9cd607907
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-17 13:08:46 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Kyle Lyver
+ * Created by: Cedric 
  * Created on: Nov 2023
  * This program moves a pixel around the microbit
 */
@@ -21,7 +21,7 @@ input.onButtonPressed(Button.A, function () {
   sprite = game.createSprite(0, 0)
 
   loopCounter = 0
-    while (loopCounter <= 3) {
+  while (loopCounter <= 3) {
     // reset ledCoordinate
     ledCoordinate = 0
     while (ledCoordinate <= 4) {
@@ -29,7 +29,7 @@ input.onButtonPressed(Button.A, function () {
       basic.pause(500)
       sprite.move(1)
       ledCoordinate++
-        }
+    }
     // tuning 90 degrees
     sprite.turn(Direction.Right, 90)
     loopCounter++
